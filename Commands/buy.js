7f76472983c2fc1acd67efef5ccae2e673f2b6a0
Cedric Lindner <66534509+Malakrovi@users.noreply.github.com>
2020-08-23 14:55:04 +0200

    Update buy.js

--- Commands/buy.js
+++ Commands/buy.js
@@ -52,14 +52,13 @@ module.exports.run = async (client, message, args) => {
             db.con.query("UPDATE members SET coins = ? WHERE id = ?", [memberCoins - parseInt(arguments[1]), message.author.id])
             db.con.query("UPDATE members SET transaction = ? WHERE id = ?", [JSON.stringify(transactionAuthor), message.author.id])
             
+             let invitecode = "";
+
             let invite = message.guild.channels.cache.filter(channel => channel.type == "text").random().createInvite({
                 maxAge: 0,
                 maxUses: 0
             }).then(invite => {
-                console.log(invite.code)
-                db.con.query("UPDATE `guilds` SET `invite`=? WHERE id = ?", [invite.code,message.guild.id], function (err, result) {
-                    if (err) throw err;
-                  });
+                invitecode = invite.code;
             })
 
             db.con.query("SELECT * FROM guilds WHERE id = ?", message.guild.id, (err, rowsServer) => {
@@ -73,7 +72,7 @@ module.exports.run = async (client, message, args) => {
                    
                    db.con.query("SELECT * FROM guilds WHERE id = ?", message.guild.id, (err, rowsServer) => {
                    	if(rowsServer.length < 1){
-                   		db.con.query("INSERT INTO `guilds` (id, name, memberCount) VALUES (?,?,?)", [message.guild.id,message.guild.name,message.guild.memberCount]);
+                   		db.con.query("INSERT INTO `guilds`(`id`, `name`, `memberCount`, `invite`, `advertName`) VALUES (?, ?, ?, ?, ?)", [message.guild.id, message.guild.name, message.guild.memberCount, invitecode, advertMessage])
                    	}else {
                    		db.con.query("UPDATE guilds SET boughtJoins = ? WHERE id = ?", [parseInt(bJ) + parseInt(arguments[1]), message.guild.id])
                     db.con.query("UPDATE guilds SET advertName = ? WHERE id = ?", [advertMessage, message.guild.id])
